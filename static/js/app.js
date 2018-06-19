055289a808382e47c5ec2c7069fbd287666285ec
knsonera <kn.sonera@gmail.com>
2018-06-19 00:16:27 -0700

    minor: preparing to get filtered places

--- static/js/app.js
+++ static/js/app.js
@@ -6,6 +6,19 @@ var ViewModel = function () {
 
     self.selectedCategory = ko.observable("All");
 
+    self.filteredPlaces = ko.computed(function () {
+        var category = self.selectedCategory();
+        if (category === "All") {
+            //loadUserPlaces(user_id, function(result) {
+            //    return result;
+            //}
+        } else {
+            //loadUserPlacesInCategory(user_id, category_id, function(result) {
+            //    return result;
+            //}
+        }
+    });
+
     _viewModel = new ViewModel();
     ko.applyBindings(_viewModel);
 }
